0fcc1dd324ca8f0da9ce4861ea834ba729a73c59
Siddhika Rakshe <siddhikarakshe@gmail.com>
2020-10-01 15:02:24 +0530

    added home icon

--- hulu-clone/src/App.js
+++ hulu-clone/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Header from "./Header";
 
 function App() {
   return (
--- hulu-clone/src/Header.js
+++ hulu-clone/src/Header.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./Header.css";
+import HomeIcon from '@material-ui/icons/Home';
+import FlashOnIcon from '@material-ui/icons/FlashOn';
+import LiveTvIcon from '@material-ui/icons/LiveTv';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
+import SearchIcon from '@material-ui/icons/Search';
+import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
 
 function Header() {
     return (
         <div className="header">
             <div className="header_icons">
                 {/* all icons here... */}
+                <HomeIcon/>
 
             </div>
             <img src="https://press.hulu.com/wp-content/uploads/2020/02/hulu-white.png " alt=" " />
